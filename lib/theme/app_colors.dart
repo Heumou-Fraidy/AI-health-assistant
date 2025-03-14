833d7037f972bb508fcae1a641d70362fedaa8e8
Heumou-Fraidy <heudan@gmail.com>
2025-03-14 20:29:05 +0100

    Adding the launching screen

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Your app's main colors
+  static const Color launchingPageGreen = Color(0xFF55833D); // Updated to your color code
+  // Add more colors as needed
+}
